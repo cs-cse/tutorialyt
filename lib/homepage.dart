b5c3ecba07717e3c498609877050221e753bcc96
Chaitanya <chaitanyaa.shrivastava@gmail.com>
2023-03-27 20:18:56 +0530

    changes made

---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  final int days = 30;
+  final String hello = "hello";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalogue App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+              "Welcome to flutter app for the $days'th day ,\n my name is $hello  "),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
