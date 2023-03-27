19a0073017ab8fccb4afe042191632f144068b38
Chaitanya <chaitanyaa.shrivastava@gmail.com>
2023-03-27 20:19:16 +0530

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tutorialyt/homepage.dart';
+import 'package:tutorialyt/Pages/homepage.dart';
+import 'package:tutorialyt/Pages/loginpage.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(primarySwatch: Colors.deepPurple),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
+  }
+
+  bringVegetables({int rupees = 100, bool thaila = false}) {
+    // take cycle
+
+    // go to market
   }
 }
