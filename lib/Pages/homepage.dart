334ead8e0aa9907d0c0169f34fbf70588c02b379
Chaitanya <chaitanyaa.shrivastava@gmail.com>
2023-04-02 12:52:05 +0530

    dynamically added items from json

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tutorialyt/models/catalog.dart';
 import 'package:tutorialyt/widgets/drawer.dart';
 import 'package:tutorialyt/widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String hello = "hello";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CataloguModal.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(50, (index) => CataloguModal.items[0]);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -20,13 +48,17 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: dummyList.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(
-                item: dummyList[index],
-              );
-            }),
+        child: CataloguModal.items != null && CataloguModal.items.isNotEmpty
+            ? ListView.builder(
+                itemCount: CataloguModal.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(
+                    item: CataloguModal.items[index],
+                  );
+                })
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -13,10 +13,29 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
+  toMap() => {
+        id: ["id"],
+        name: "name",
+        desc: "desc",
+        price: "price",
+        color: "color",
+        image: "image",
+      };
 }
 
 class CataloguModal {
-  static final items = [
+  static List<Item> items = [
     Item(
         id: 1,
         name: "iPhone 12 Pro",
